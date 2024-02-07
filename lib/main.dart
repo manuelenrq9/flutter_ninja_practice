7bc79652ed655e468de9563e1a3a2f8ad3871053
manuelenrq9 <manuel.enrqt@gmail.com>
2024-02-07 18:45:09 -0400

    Hot-reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Practica de Flutter'),
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Practica de Flutter'),
+        centerTitle: true,
+        backgroundColor: Colors.orange[300],
+      ),
+      body: Center(
+        child: Text(
+          'Hola a todos como estan',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 1.2,
+            color: Colors.grey[700],
+            fontFamily: 'IndieFlower',
+          ),
         ),
       ),
-    ));
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.orange[300],
+        child: Text('Click!'),
+        onPressed: null,
+      ),
+    );
+  }
+}
