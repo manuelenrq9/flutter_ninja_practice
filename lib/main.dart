b6a5a7779a8a13342248b4f32cfcd4798b9e5f4d
manuelenrq9 <manuel.enrqt@gmail.com>
2024-02-20 12:18:14 -0400

    ep 11

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,19 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.orange[300],
       ),
-      body: Center(
-        child: Image.asset('assets/2.jpg'),
-      ),
+      body: Row(children: <Widget>[
+        Text('Hello World'),
+        ElevatedButton(
+            onPressed: () {},
+            child: Text('Click me'),
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.amber))),
+        Container(
+          color: Colors.cyan,
+          padding: EdgeInsets.all(30),
+          child: Text('inside container'),
+        )
+      ]),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.orange[300],
         child: Text('Click!'),
